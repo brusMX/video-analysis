04d8bb6d7e5dd0924ece10136bad7146a641dd2b
Justin Wendlandt <jwendl@hotmail.com>
2018-02-13 14:50:48 -0800

    Updating class to support more services.

--- ComputerVisionExample/ComputerVisionExample/Services/ImageCaptureService.cs
+++ ComputerVisionExample/ComputerVisionExample/Services/ImageCaptureService.cs
@@ -1,21 +1,23 @@
 ﻿using ComputerVisionExample.Models;
-using Microsoft.ProjectOxford.Common;
 using Microsoft.ProjectOxford.Common.Contract;
 using Microsoft.ProjectOxford.Emotion;
 using Microsoft.ProjectOxford.Face;
 using Microsoft.ProjectOxford.Vision;
+using Microsoft.ProjectOxford.Vision.Contract;
 using OpenCvSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Face = Microsoft.ProjectOxford.Face.Contract.Face;
+using Rectangle = Microsoft.ProjectOxford.Common.Rectangle;
 
 namespace ComputerVisionExample
 {
     public interface IImageCaptureService
     {
-
+        Task FetchVideoFromCameraAsync(int cameraIndex = 0, double overrideFps = 0);
     }
 
     public class ImageCaptureService
@@ -28,7 +30,6 @@ namespace ComputerVisionExample
         private readonly IEmotionServiceClient emotionServiceClient;
         private readonly IFaceServiceClient faceServiceClient;
         private readonly IVisionServiceClient visionServiceClient;
-        private readonly Timer timer;
 
         public ImageCaptureService()
         {
@@ -38,8 +39,10 @@ namespace ComputerVisionExample
         }
 
         public IEnumerable<Emotion> Emotions { get; set; }
+        public IEnumerable<Face> Faces { get; set; }
+        public AnalysisResult AnalysisResult { get; set; }
 
-        public void FetchVideoFromCamera(int cameraIndex = 0, double overrideFps = 0)
+        public async Task FetchVideoFromCameraAsync(int cameraIndex = 0, double overrideFps = 0)
         {
             var videoCapture = new VideoCapture(cameraIndex);
             var fps = overrideFps;
@@ -48,7 +51,7 @@ namespace ComputerVisionExample
                 fps = 30;
             }
 
-            StartProcessing(videoCapture, 0, TimeSpan.FromSeconds(1 / fps), () => DateTime.Now);
+            await Task.Factory.StartNew(() => StartProcessing(videoCapture, 0, TimeSpan.FromSeconds(1 / fps), () => DateTime.Now));
         }
 
         protected void StartProcessing(VideoCapture videoCapture, int frameCount, TimeSpan delay, Func<DateTime> timestampFunction)
@@ -57,29 +60,52 @@ namespace ComputerVisionExample
             {
                 await Task.Factory.StartNew(async () =>
                 {
+                    var capture = state as VideoCapture;
                     var timestamp = timestampFunction();
                     var image = new Mat();
 
                     VideoFrameMetadata videoFrameMetadata;
                     videoFrameMetadata.Index = frameCount;
                     videoFrameMetadata.Timestamp = timestamp;
-                    videoCapture.Read(image);
+                    capture.Read(image);
 
                     var videoFrame = new VideoFrame(image, videoFrameMetadata);
                     await AnalyzeEmotionAsync(videoFrame);
                 });
-            });
+            }, videoCapture, TimeSpan.Zero, delay);
         }
 
-        protected async Task AnalyzeFace(VideoFrame videoFrame)
+        protected async Task AnalyzeFaceAsync(VideoFrame videoFrame)
         {
+            var imageStream = videoFrame.Image.ToMemoryStream(".jpg", jpegParameters);
+            var faceAttributes = new List<FaceAttributeType>()
+            {
+                FaceAttributeType.Age,
+                FaceAttributeType.Gender,
+                FaceAttributeType.HeadPose
+            };
+            Faces = await faceServiceClient.DetectAsync(imageStream, returnFaceAttributes: faceAttributes);
+        }
 
+        protected async Task AnalyzeVisionAsync(VideoFrame videoFrame)
+        {
+            var imageStream = videoFrame.Image.ToMemoryStream(".jpg", jpegParameters);
+            var visualFeatures = new List<VisualFeature>()
+            {
+                VisualFeature.Adult,
+                VisualFeature.Categories,
+                VisualFeature.Description,
+                VisualFeature.Tags,
+                VisualFeature.Color,
+                VisualFeature.ImageType,
+            };
+            var analysisResult = await visionServiceClient.AnalyzeImageAsync(imageStream, visualFeatures);
         }
 
         protected async Task AnalyzeEmotionAsync(VideoFrame videoFrame)
         {
             var imageStream = videoFrame.Image.ToMemoryStream(".jpg", jpegParameters);
-            var localFaces = (Rect[])videoFrame.UserData;
+            var localFaces = (IEnumerable<Rect>)videoFrame.UserData;
             if (localFaces == null)
             {
                 // If localFaces is null, we're not performing local face detection.
